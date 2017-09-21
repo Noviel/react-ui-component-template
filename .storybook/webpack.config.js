b6902fb84af8bdb29b6b949a81e4ef0d07cd61a6
Aleksandr Snov <snov.contact@gmail.com>
2017-09-22 00:41:41 +0700

    Fix storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const paths = require('../webpack/paths');
 
 const { createStyleRules } = require('../webpack/rules/style');
 
 module.exports = (storybookBaseConfig, configType) => {
   storybookBaseConfig.module.rules = storybookBaseConfig.module.rules.concat(
-    createStyleRules({ target: 'client', production: false })
+    createStyleRules({ target: 'client', production: false }, {
+      cssModulesPath: paths.components
+    })
   );
 
   return storybookBaseConfig;
